7a5209c8aa68e585347f0b4d0203207ebbc939f7
Sepahrad Dadash <sepahrad.dadash@learnet.se>
2024-12-11 09:50:48 +0100

    fängelse

---
+++ fängelse.py
@@ -0,0 +1,6 @@
+def steg_ett():
+    print("de heter bashar epstien diddy de är gripen för 2st våltäkt 5st kidnapping 3st misshandel skolsjutning 345 mord")
+    print("1. ring till advokat")
+    print("2. vänta ")
+    choice= input("välja 1 eller 2: ")
+    
